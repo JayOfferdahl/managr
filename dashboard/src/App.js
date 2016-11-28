8c60aeb8b8cc10acdc63500a43a7d20be3bb8948
Evan Nichols <evannichols@MacBook-Pro-28.local>
2016-11-28 13:16:28 -0600

    add nav + icons, start body

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import logo from '../img/logo.png';
+import avatar from '../img/avatar.png';
+
+/*
+NAVIGATION ICONS
+*/
+import milestoneIcon from "../img/milestone-icon.png";
+import financeIcon from "../img/finance-icon.png";
+import docsIcon from "../img/docs-icon.png";
+import projectIcon from "../img/project-icon.png";
+
 import {
+  FormControl,
   Row,
   Col,
   Button,
@@ -7,21 +19,72 @@ import {
   NavItem,
   Grid
 } from 'react-bootstrap';
-import logo from '../img/logo.png';
+
+//CSS Stuff
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        dataObj: null
+      };
+    }
+
+    render() {
+      return(
+
+        <div className="App">
+
+          <div className="App-body">
+          <Grid fluid={true}>
+            <Row>
+              <Col className="navbar" xs={4} md={2}>
+                <div className="avatar-section">
+                  <img src={avatar} className="avatar-icon" alt="avatar" />
+                    <div className="avatar-text">
+                      <p><b>John Smith</b></p>
+                      <p> Project Lead 2</p>
+                    </div>
+                    <hr/>
+                </div>
+                <Nav className="navigation" bsStyle="pills" stacked>
+                  <NavItem eventKey={1}>
+                    <img src={projectIcon} className="nav-icon" alt="project" />
+                    Project
+                  </NavItem>
+                  <NavItem eventKey={2}>
+                    <img src={milestoneIcon} className="nav-icon" alt="milestoens" />
+                    Milestones
+                  </NavItem>
+                  <NavItem eventKey={3}>
+                    <img src={financeIcon} className="nav-icon" alt="finance" />
+                    Finance
+                  </NavItem>
+                  <NavItem eventKey={4}>
+                    <img src={docsIcon} className="nav-icon" alt="documents" />
+                    Documents
+                  </NavItem>
+                </Nav>
+              </Col>
+              <Col xs={14} md={10}>
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+                <Row className="project-overview-section">
+                  <p className="project-title">Westminister School Renovation</p>
+                  <p>Project Progress: <b>63%</b></p>
+                  <p>Active Milestones: <b>Lay Foundation, Drywall</b></p>
+                  <p>Client POC: <b>Debbie Walles</b> (913) 787 6969</p>
+                </Row>
+                <p>Here is the content</p>
+              </Col>
+            </Row>
+          </Grid>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
