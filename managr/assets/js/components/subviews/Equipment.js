e1dc2b5526ebfe432e53d46c2c47ec810fea48ef
Evan Nichols <evannichols@MacBook-Pro-28.local>
2017-01-31 19:47:00 -0600

    restructured components folder, adding routing for subviews

---
+++ managr/assets/js/components/subviews/Equipment.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import '../App.css'
+
+export default class Equipment extends Component {
+  render () {
+    return <h3 className="placeholder">This will be the Equipment tab</h3>
+  }
+}
