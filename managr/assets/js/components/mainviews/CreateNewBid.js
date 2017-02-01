b91b372926b3f7e61641ac16d2b69b10bd3302b6
Evan Nichols <evannichols@MacBook-Pro-28.local>
2017-01-31 19:52:46 -0600

    remove useless folders

--- managr/assets/js/components/mainviews/CreateNewBid.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
-import '../App.css'
-
-export default class CreateNewBid extends Component {
-  render () {
-    return <h3 className="placeholder">This will be the Create New Bid tab</h3>
-  }
-}
