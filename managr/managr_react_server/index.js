b47c90f406ee50c45befc397e3ae436bc57d266a
Ben Davidson <davidsonben0@gmail.com>
2017-02-13 15:19:42 -0600

    Beginnings of Express Server

---
+++ managr/managr_react_server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+let app = express();
+
+app.get('/*', (req,res) => {
+	res.send('Hello World!');
+});
+
+app.listen(8080, () => console.log('Running on localhost:8080'));
