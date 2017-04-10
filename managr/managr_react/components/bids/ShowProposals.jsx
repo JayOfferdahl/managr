f1e50d59b48e3dbc52b02123bbdd80c84c8ffb93
Adam Thompson <theadamjthompson@gmail.com>
2017-04-09 21:24:39 -0500

    fix numeric sort

--- managr/managr_react/components/bids/ShowProposals.jsx
+++ managr/managr_react/components/bids/ShowProposals.jsx
@@ -13,6 +13,14 @@ const options = {
   }
 };
 
+function numericSortFunc(a, b, order) {
+  if (order === 'desc') {
+    return Number(b.budget) - Number(a.budget);
+  } else {
+    return Number(a.budget) - Number(b.budget);
+  }
+};
+
 class ShowProposals extends React.Component {
   componentWillMount() {
     this.props.loadProposalsFromServer();
@@ -29,7 +37,7 @@ class ShowProposals extends React.Component {
         <BootstrapTable data = { this.props.proposals } options = { options } striped hover pagination>
             <TableHeaderColumn isKey = { true } dataSort={ true } filter={ { type: 'TextFilter'} } dataField='name'>Name</TableHeaderColumn>
             <TableHeaderColumn dataSort={ true } filter={ { type: 'TextFilter'} } dataField='location'>Location</TableHeaderColumn>
-            <TableHeaderColumn dataSort={ true } dataField='budget'>Budget</TableHeaderColumn>
+            <TableHeaderColumn dataSort={ true } sortFunc={ numericSortFunc } dataField='budget'>Budget</TableHeaderColumn>
             <TableHeaderColumn dataSort={ true } dataField='start'>Start Date</TableHeaderColumn>
             <TableHeaderColumn dataSort={ true } dataField='end'>End Date</TableHeaderColumn>
         </BootstrapTable>
