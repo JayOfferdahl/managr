773a14dd79450e925892a439046ca2635fa49262
Adam Thompson <theadamjthompson@gmail.com>
2017-04-09 21:15:25 -0500

    add sort and filter to table
    number filtering is buggy
    date filtering will need work, date format for table differs from what i'm giving it

--- managr/managr_react/components/bids/ShowProposals.jsx
+++ managr/managr_react/components/bids/ShowProposals.jsx
@@ -5,23 +5,38 @@ import { connect } from 'react-redux';
 import { loadProposalsFromServer } from '../../actions/ShowProposalsActions';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
+const options = {
+  onRowClick: function(row) {
+    //alert(`You click row id: ${row.name}`);
+    var getURL = window.location;
+    window.location.href = getURL.protocol + "//" + getURL.host + "/proposal/" + row.name;
+  }
+};
 
 class ShowProposals extends React.Component {
   componentWillMount() {
     this.props.loadProposalsFromServer();
   };
+  /*constructor(props) {
+    super(props);
+    this.options = {
+      defaultSortName: 'name',  // default sort column name
+      defaultSortOrder: 'desc',  // default sort order
+    };
+  };*/
   render () {
       return (
-            <BootstrapTable data= { this.props.proposals } striped hover>
-                <TableHeaderColumn isKey dataField='name'>Name</TableHeaderColumn>
-                <TableHeaderColumn dataField='location'>Location</TableHeaderColumn>
-                <TableHeaderColumn dataField='budget'>Budget</TableHeaderColumn>
-                <TableHeaderColumn dataField='start'>Start Date</TableHeaderColumn>
-                <TableHeaderColumn dataField='end'>End Date</TableHeaderColumn>
-            </BootstrapTable>
-        );
+        <BootstrapTable data = { this.props.proposals } options = { options } striped hover pagination>
+            <TableHeaderColumn isKey = { true } dataSort={ true } filter={ { type: 'TextFilter'} } dataField='name'>Name</TableHeaderColumn>
+            <TableHeaderColumn dataSort={ true } filter={ { type: 'TextFilter'} } dataField='location'>Location</TableHeaderColumn>
+            <TableHeaderColumn dataSort={ true } dataField='budget'>Budget</TableHeaderColumn>
+            <TableHeaderColumn dataSort={ true } dataField='start'>Start Date</TableHeaderColumn>
+            <TableHeaderColumn dataSort={ true } dataField='end'>End Date</TableHeaderColumn>
+        </BootstrapTable>
+      );
     }
 };
+
 const mapStateToProps = (state) => {
     return {
         proposals: state.proposals,
