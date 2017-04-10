dbfb9d1e7e30954b7779ebf9e845afb70f65a60f
Jay Offerdahl <jaysanco@gmail.com>
2017-04-10 01:47:09 -0500

    Remove checklist text.

--- managr/managr_react/components/proposals/CreateProposal.jsx
+++ managr/managr_react/components/proposals/CreateProposal.jsx
@@ -12,21 +12,6 @@ class CreateProposal extends React.Component {
                 <h2>Create a new project proposal</h2>
                 <p>To create a new proposal, fill out the following form, and your proposal will be able to accept bids from our network!</p>
                 <ProposalForm />
-                <br/>
-
-                <div className="todo">
-                    <p>We need to complete the following tasks here:</p>
-                    <ul>
-                        <li><strike>Add form which accepts relevant project input.</strike></li>
-                        <li><strike>Connect the form from the front end to the server (which will create the proposal object)</strike></li>
-                        <li><strike>Correctly validate the form & provide meaningful error messages.</strike></li>
-                        <li><strike>style the page.</strike></li>
-                        <ul>
-                            <li><strike>style initial form.</strike></li>
-                            <li><strike>style form response/errors.</strike></li>
-                        </ul>
-                    </ul>
-                </div>
             </Row>
         )
     };
