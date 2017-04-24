7c22b76bce2fb8c393194f8199d45fd45c4667af
Adam Thompson <theadamjthompson@gmail.com>
2017-04-24 00:39:10 -0500

    Change summary tables to boost-react-table
    Altered code and mock data

--- managr/managr_react/components/project/Activity.jsx
+++ managr/managr_react/components/project/Activity.jsx
@@ -2,25 +2,65 @@ import React from 'react';
 
 import moment from 'moment';
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+import '../../assets/css/react-bootstrap-table.css';
+
 import ActivityTable from './ActivityTable'
 
 import '../../assets/css/App.css';
 
 const MOCK_HEADERS = ['Result','Message','Timestamp']
 
-const MOCK_DATA_ROWS = [{ 'Message': 'John Smith logged 8.5 hours onto the project.',
-                          'Timestamp': moment().format('MMMM Do YYYY, h:mm:ss a'),
+const MOCK_DATA_ROWS = [{ 'Event': 'John Smith logged 8.5 hours onto the project.',
                         },
-                        { 'Message': 'DitchWitch Driller used for 4.5 hours',
-                          'Timestamp': moment().format('MMMM Do YYYY, h:mm:ss a'),
+                        { 'Event': 'DitchWitch Driller used for 4.5 hours',
                         },
-                        { 'Message': 'Brick purchase receipt added to files.',
-                          'Timestamp': moment().format('MMMM Do YYYY, h:mm:ss a'),
+                        { 'Event': 'JLG 1500SJ Telescopic Boom Lift #1 used for 5 hours',
+                        },
+                        { 'Event': 'JLG 1500SJ Telescopic Boom Lift #3 9 hours',
+                        },
+                        { 'Event': 'JLG 660SJC Telescopic Crawler Boom Lift down for maintenance',
+                        },
+                        { 'Event': 'Johnny on the Spot cleaning',
+                        },
+                        { 'Event': 'Snow day',
+                        },
+                        { 'Event': 'Sublevel pouring complete',
+                        },
+                        { 'Event': 'CAT TL943D Telehandler #2 returned from maintenance',
+                        },
+                        { 'Event': 'OSHA suprise visit',
+                        },
+                        { 'Event': 'Incident: Jay Offerdahl suckerpunched Chad Papineau for no reason',
+                        },
+                        { 'Event': 'Struck oil while excavating sublevel for west compound',
+                        },
+                        { 'Event': 'Incident: generator #3 overloaded',
+                        },
+                        { 'Event': 'Brick purchase receipt added to files.',
                         }]
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function _addDate(row) {
+  row['Timestamp'] = randomDate(new Date(2017, 1, 1), new Date(2017,5,5));
+  return row;
+}
 
 class Activity extends React.Component {
     render () {
-        return <ActivityTable headers={MOCK_HEADERS} rows={MOCK_DATA_ROWS} />
+      MOCK_DATA_ROWS.map(_addDate)
+      MOCK_DATA_ROWS.push({'Event': 'The Undertaker threw Mankind off Hell In A Cell, and plummeted 16 ft through an announcer’s table',
+                              'Timestamp': 'Sun June 28 1998 20:43:33 GMT-0500 (CDT)'});
+      return (
+      <BootstrapTable data={MOCK_DATA_ROWS} striped hover pagination>
+           <TableHeaderColumn isKey={true} dataSort={true} dataField='Timestamp'>Timestamp</TableHeaderColumn>
+           <TableHeaderColumn dataSort={true} dataField='Event'>Event</TableHeaderColumn>
+      </BootstrapTable>
+    )
     }
 }
 
--- managr/managr_react/components/project/Materials.jsx
+++ managr/managr_react/components/project/Materials.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import MaterialsTable from './MaterialsTable'
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+import '../../assets/css/react-bootstrap-table.css';
+
 import '../../assets/css/App.css';
 
 const MOCK_HEADERS = ['Result','Material Name','Unit','Cost Per Unit','Total Usage','Total Expense']
@@ -21,12 +25,77 @@ const MOCK_DATA_ROWS = [{ 'Material Name': 'Bricks',
                           'Cost Per Unit': 36.10,
                           'Total Usage': 45,
                         },
+                        { 'Material Name': 'Cookie Dough',
+                          'Unit': 'buckets',
+                          'Cost Per Unit': 20,
+                          'Total Usage': 850,
+                        },
+                        { 'Material Name': 'Diesel, Offroad',
+                          'Unit': 'gallons',
+                          'Cost Per Unit': 3.61,
+                          'Total Usage': 450,
+                        },
+                        { 'Material Name': 'Diesel',
+                          'Unit': 'gallons',
+                          'Cost Per Unit': 4.34,
+                          'Total Usage': 1824,
+                        },
+                        { 'Material Name': 'Bacon',
+                          'Unit': 'hawg lbs',
+                          'Cost Per Unit': 5,
+                          'Total Usage': 4500,
+                        },
+                        { 'Material Name': 'R13 Insulation',
+                          'Unit': 'cubic ft.',
+                          'Cost Per Unit': 0.43,
+                          'Total Usage': 9023,
+                        },
+                        { 'Material Name': 'Asbestos',
+                          'Unit': 'lungs',
+                          'Cost Per Unit': .01,
+                          'Total Usage': 999999999,
+                        },
+                        { 'Material Name': 'Salt',
+                          'Unit': 'pillars',
+                          'Cost Per Unit': 69,
+                          'Total Usage': 420,
+                        },
+                        { 'Material Name': 'Asphalt',
+                          'Unit': 'tons',
+                          'Cost Per Unit': 63.01,
+                          'Total Usage': 54,
+                        },
+                        { 'Material Name': 'Toilet Paper',
+                          'Unit': 'miles',
+                          'Cost Per Unit': 2500,
+                          'Total Usage': 990,
+                        },
+
                       ]
+function _addCost(row) {
+  row['Total Cost'] = "$" + Math.round(row['Cost Per Unit'] * row['Total Usage'] * 100)/100 ;
+  row['Cost Per Unit'] = "$" + row['Cost Per Unit'];
+  return row;
+}
 
 class Materials extends React.Component {
-    render () {
-        return <MaterialsTable headers={MOCK_HEADERS} rows={MOCK_DATA_ROWS}/>
-    }
+    // render () {
+    //     return <MaterialsTable headers={MOCK_HEADERS} rows={MOCK_DATA_ROWS}/>
+    // }
+
+    render() {
+      MOCK_DATA_ROWS.map(_addCost);
+
+    return (
+         <BootstrapTable data={MOCK_DATA_ROWS} striped hover pagination>
+              <TableHeaderColumn isKey={true} dataSort={true} dataField='Material Name'>Material Name</TableHeaderColumn>
+              <TableHeaderColumn dataSort={true} dataField='Unit'>Unit</TableHeaderColumn>
+              <TableHeaderColumn dataSort={true} dataField='Cost Per Unit'>Cost Per Unit</TableHeaderColumn>
+              <TableHeaderColumn dataSort={true} dataField='Total Usage'>Total Usage</TableHeaderColumn>
+              <TableHeaderColumn dataSort={true} dataField='Total Cost'>Total Cost</TableHeaderColumn>
+         </BootstrapTable>
+    );
+  }
 }
 
 export default Materials;
